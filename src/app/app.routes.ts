9fa59ea74f2e006711e0e4ae8b4c6f9a2a1f3c70
Dennis7456 <denniskiplangat.dk@gmail.com>
2024-05-06 19:41:54 +0300

    auth guard services for authorised users and unauthorised users

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,19 +3,24 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { GuestGuardService } from './services/guest-guard.service';
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent,
+        canActivate: [GuestGuardService]
     },
     {
         path: 'register',
         component: RegisterComponent,
+        canActivate: [GuestGuardService]
     },
     {
         path: 'dashboard',
         component: DashboardComponent,
+        canActivate: [AuthGuardService]
     },
     {
         path: '',
---
+++ src/app/services/guest-guard.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuestGuardService {
+
+  constructor(
+    private router: Router
+    ) 
+  { }
+
+  canActivate(
+
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+    ) {
+    if (
+      typeof localStorage !== 'undefined' && !localStorage.getItem('access_token')
+      ) { return true; }
+      this.router.navigateByUrl('/');
+      return false;
+  }
+}
